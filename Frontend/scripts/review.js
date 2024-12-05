9d813afc1f8397a2463d85edf7fb1f244e2a92f3
cherishma23 <115597751+cherishma23@users.noreply.github.com>
2024-12-04 17:13:30 -0800

    Your commit message

--- Frontend/scripts/review.js
+++
@@ -1,8 +0,0 @@
-function toggleForm() {
-    const form = document.getElementById('reviewForm');
-    if (form.style.display === 'none' || form.style.display === '') {
-        form.style.display = 'block'; // Show the form
-    } else {
-        form.style.display = 'none'; // Hide the form
-    }
-}
